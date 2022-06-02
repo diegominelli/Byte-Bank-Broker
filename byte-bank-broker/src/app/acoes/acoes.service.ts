0c8319e3412d1ce0763ca02a541894d68e87a544
Diego Minelli <diego.minelli@hotmail.com>
2022-06-02 20:01:24 -0300

    usando o tap e o pluck

--- byte-bank-broker/src/app/acoes/acoes.service.ts
+++ byte-bank-broker/src/app/acoes/acoes.service.ts
@@ -1,7 +1,7 @@
 import { Acao } from './modelo/acoes';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { map } from 'rxjs/operators';
+import { map, pluck, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -10,13 +10,13 @@ export class AcoesService {
   constructor(private httpClient: HttpClient) {}
 
   getAcoes() {
-    return this.httpClient
-      .get<any>('http://localhost:3000/acoes')
-      .pipe(
-        map((acoes) =>
-          acoes.sort((acaoA, acaoB) => this.ordenaPorCodigo(acaoA, acaoB))
-        )
-      );
+    return this.httpClient.get<any>('http://localhost:3000/acoes').pipe(
+      tap((valor) => console.log(valor)),
+      pluck('payload'),
+      map((acoes) =>
+        acoes.sort((acaoA, acaoB) => this.ordenaPorCodigo(acaoA, acaoB))
+      )
+    );
   }
 
   private ordenaPorCodigo(acaoA: Acao, acaoB: Acao) {
